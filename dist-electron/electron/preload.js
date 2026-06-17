659c7581c61a2d9ac804d878f8f9c669057abafd
ritesh-avkalan <ritesh.avkalan@gmail.com>
2026-06-17 23:41:16 +0530

    style: update UI to Atom One theme and refactor electron preload listener handling

--- dist-electron/electron/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electronAPI', {
-    platform: process.platform,
-    appName: 'ESD Desktop',
-});
-//# sourceMappingURL=preload.js.map
